891c9b46f5df2273d21a9c37716d476b5a50e464
daniel.jodar <9wrZR9Gq6TXs>
2017-10-10 12:12:20 +0200

    Open account in testing

--- src/test/scala/com/djodar/katas/algebra_api/AccountServiceTest.scala
+++ src/test/scala/com/djodar/katas/algebra_api/AccountServiceTest.scala
@@ -10,16 +10,15 @@ import scala.util.Success
 object AccountServiceTest extends Properties("AccountService") {
 
   val amounts: Gen[BigDecimal] = Gen.choose(-1000, 1000).map(BigDecimal(_))
-  val accounts: Gen[Account] = for (n <- amounts) yield Account("anyNo", "anyName", LocalDate.now().plusDays(1), balance = n)
 
-  property("Equal credit & debit in sequence retain the same balance") = forAll(accounts, amounts) {
-    (account: Account, amount: BigDecimal) =>
-      val Success((before, after)) = for {
-        b <- AccountService.balance(account)
-        c <- AccountService.credit(account, amount)
-        d <- AccountService.debit(c, amount)
-      } yield (b, d.balance)
+  property("Equal credit & debit in sequence retain the same balance") = forAll(amounts) { amount: BigDecimal =>
+    val Success((before, after)) = for {
+      a <- AccountService.open("anyNo", "anyName", Some(LocalDate.now().plusDays(1)))
+      b <- AccountService.balance(a)
+      c <- AccountService.credit(a, amount)
+      d <- AccountService.debit(c, amount)
+    } yield (b, d.balance)
 
-      before == after
+    before == after
   }
 }
